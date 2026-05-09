5409a614df20454ee0c8191ba832deed49bc80e3
Alex <azhidenko13@gmail.com>
2026-05-09 22:54:29 +0300

    feat: celery settings

--- src/core/config.py
+++ src/core/config.py
@@ -6,6 +6,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     database_url: PostgresDsn
+    celery_broker_url: str
+    celery_result_backend: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
---
+++ src/tasks/celery_app.py
@@ -0,0 +1,19 @@
+from celery import Celery
+
+from src.core.config import settings
+
+celery_app = Celery(
+    "production_control",
+    broker=settings.celery_broker_url,
+    backend=settings.celery_result_backend,
+    include=["src.tasks.batch_tasks"]
+)
+
+celery_app.conf.update(
+    task_track_started=True,
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+    timezones="UTC",
+    enable_utc=True
+)
